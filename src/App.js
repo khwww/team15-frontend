6e39ecafcb571a57c5284cc90aa9b6e0dd6303ef
Jiwon Park <128890692+99JIWONDEV@users.noreply.github.com>
2025-03-18 17:52:44 +0900

    Revert "Feat/publishing"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React from "react"
-import GlobalStyle from "./lib/styles/GlobalStyle"
-import Router from "./shared/Router"
-import theme from "./lib/styles/theme"
-import { ThemeProvider } from "styled-components"
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-
 function App() {
   
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <div className="App">
+      test2
+    </div>
   )
 }
 
-export default App;
+export default App
